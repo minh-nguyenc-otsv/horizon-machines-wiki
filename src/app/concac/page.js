6a02712b45db927ba23f4b069b65f9f00c913a4d
minh-nguyenc-otsv <minh.nguyenc@one-line.com>
2023-05-30 17:38:50 +0700

    draft layout.js

---
+++ src/app/concac/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function ConCac() {
+	return (
+		<div>
+			<h1>CON CAC</h1>
+			<Image src='/horizon_machines.jpeg' alt='horizon machines' fill={true}/>
+		</div>
+
+	)
+}
