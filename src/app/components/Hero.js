68833729fd07f1900e8c324ab75e0cba3f03fb79
AntoineAT <nguyencuutoc@gmail.com>
2023-06-02 01:12:30 +0700

    implement 'how to fight' modal.

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,33 +1,47 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import Modal from "./Modal";
 
-
-export default function Hero() {
-  return (
-    <div className="hero bg-transparent">
-      <div className="hero-content flex-col lg:flex-row">
-        <Image
-          src="/Thunderjaw.png"
-          alt="machines"
-          width="0"
-          height="0"
-          sizes="100vw"
-          quality={100}
-          className="w-full h-auto rounded-full"
-        />
-        <div className="text-black">
-          <h1 className="text-5xl font-bold">Thunderjaw</h1>
-          <p className="py-6">
-            Apex combat machine, equipped with a wide array of heavy artillery
-            and melee attacks.
-          </p>
-          <button className="btn btn-primary" onClick={()=>window.my_modal.showModal()}>Components</button>
-          <button className="btn btn-primary" onClick={()=>window.my_modal.showModal()}>How to fight</button>
-          <Modal/>
-        </div>
-      </div>
-    </div>
-  );
+export default function Hero({ machine, machine_components }) {
+	// const machine_name = "Thunderjaw";
+	// const machine_info =
+	// 	"Apex combat machine, equipped with a wide array of heavy artillery and melee attacks.";
+	return (
+		<div className="hero bg-transparent">
+			<div className="hero-content flex-col lg:flex-row">
+				<Image
+					src="/Thunderjaw.png"
+					alt="machines"
+					width="0"
+					height="0"
+					sizes="100vw"
+					quality={100}
+					className="w-full h-auto rounded-full"
+				/>
+				<div className="text-black space-x-2 space-y-2">
+					<h1 className="text-5xl font-bold">{machine["machine_name"]}</h1>
+					<p className="py-6">{machine["how_to_fight"]}</p>
+					<div className="grid grid-cols-2 grid-row-1 gap-4">
+						<button
+							className="btn btn-primary"
+							onClick={() => window.my_modal.showModal()}
+						>
+							Components
+						</button>
+						<button
+							className="btn btn-primary"
+							onClick={() => window.my_modal.showModal()}
+						>
+							How to fight
+						</button>
+					</div>
+					<Modal
+						machine_components={machine_components}
+						how_to_fight={machine["how_to_fight"]}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
 import Carousel from "../app/components/Carousel";
 import Hero from "./components/Hero";
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 async function fetchMachine(machine_name) {
-    const prisma = new PrismaClient();
-    const machine = await prisma.machines.findFirst({
-        where: {
-            machine_name: machine_name,
-        },
-    });
-    console.log(machine);
-    return machine;
+	const prisma = new PrismaClient();
+	const machine = await prisma.machines.findFirst({
+		where: {
+			machine_name: machine_name,
+		},
+	});
+	console.log(machine);
+	return machine;
 }
 
-export default function Home() {
-  return (
-    <div>
-      <div>
-        <Hero />
-      </div>
-      <div>
-        <Carousel />
-      </div>
-    </div>
-  );
+export default async function Home() {
+	const machine = await fetchMachine("Thunderjaw");
+	console.log(machine);
+	return (
+		<div>
+			<div>
+				<Hero machine={machine} machine_components={""} />
+			</div>
+			<div>
+				<Carousel />
+			</div>
+		</div>
+	);
 }
